b143141f9b34503535ccac469aaa55c557289b15
BrnCalisario <bruno145224@gmail.com>
2022-11-25 17:00:13 -0300

    erros e mais erros

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+let ejs = require('ejs')
+let fs = require('fs')
 
 const app = express()
 
@@ -8,6 +10,16 @@ app.use(express.static(__dirname + '/css'))
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"))
+})
+
+app.get("/pokemon/:id", (req, res) => {
+    const poke_id = req.params.id
+    console.log(JSON.stringify(poke_id))
+
+    fs.readFile('pokemon.html', 'utf-8', (err, html) => {
+        res.send(ejs.render(html.toString(), {poke_id: poke_id}))
+    })
+
 })
 
 
