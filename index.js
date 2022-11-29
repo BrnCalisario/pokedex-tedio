3aed2b130959679a1392278f74792e110c66dc3f
BrnCalisario <bruno145224@gmail.com>
2022-11-29 11:51:32 -0300

    Implementando botoes de filtro

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const express = require("express")
-const { is } = require("type-is")
+const path = require("path")
 
 const app = express()
 
 app.use(express.static(__dirname + '/js'))
-app.use(express.static(__dirname + '/css'))
+app.use(express.static(path.join(__dirname, '/css')))
     
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-
 app.get("/pokemon/:id", (req, res) => {
     const { id } = req.params
 
-    
     if (id < 0 || !isNumber(id)) {
         //res.sendFile(__dirname + "notfound.html")
         res.send('<h1> 404 Not Found </h1>')
     }
 
-    
     res.sendFile(__dirname + "/pokemon.html")
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."))
-
-
-
-
 
 function isNumber(char) {
     if (typeof char !== 'string') {
--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ async function getPokemons() {
 async function renderPokemons(pokeList) {
     
     $("#field").empty()
-
+    
     for(var pokeResponse of pokeList) {
         
         var pokemon = await getSinglePokemon(pokeResponse.url)
@@ -49,28 +49,97 @@ async function getSinglePokemon(url) {
 async function searchPokemon() {
     var input = $("#search-input").val()
 
+    $("#search-btn").prop("disabled", true)
     var pokemons = await getPokemons()
-
-    if (input.length < 1) {
-        renderPokemons(pokemons)
-        return
-    }
 
     var filteredSearch = pokemons.filter( p => p.name.includes(input))
 
     if (filteredSearch.length == 0) {
         renderError()
+        $("#search-btn").prop("disabled", false)
         return
     }
 
-    renderPokemons(filteredSearch)
+    renderPokemons(filteredSearch).then( () => {
+            $("#search-btn").prop("disabled", false)
+        }
+    )
+}
+
+
+async function filterType(type) {
+    var all = await getPokemons()
+
+    var filtered = await all.filter( pokeResponse => {
+        return true
+    })
+
+
+
+
+    // filtered = await all.forEach(async response => {
+    //     var pokemon = await getSinglePokemon(response.url)
+    //     var { types } = pokemon 
+
+
+    //     var find = false
+    //     types.forEach(pokeType => {
+    //         if (pokeType.type.name == type && !find) {
+                
+    //             filtered.push(response)
+    //             console.log(pokeType.type.name, response.name)
+                
+    //             find = true
+    //         }
+    //     })
+    // })
+
+    console.log(filtered)
+
+    return await filtered
+}
+
+const applyBtnColor = () => {
+    $("#btn-list > button").each(function () { 
+        let element = $(this).prop("innerHTML")
+        switch(element) {
+            case "Normal":
+                $(this).css("color", "black")
+                break
+            case "Fire":
+                $(this).css("background-color", "#d9372b")
+                break
+            case "Grass":
+                $(this).css("background-color", "#49b85a")
+                break
+            case "Water":
+                $(this).css("background-color", "#2b8bd9")
+                break
+            case "Ground":
+                $(this).css("background-color", "#d9742b")
+        }
+    })
+}
+
+const btnFunction = () => {
+    $("#btn-list > button").each(function () {
+        let button = $(this)
+
+        button.click(async function () {
+            await renderPokemons(await filterType(button.val()))
+        })
+        
+    })
 }
 
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1)
 
 var start = async () => {
-    renderPokemons(await getPokemons())
+    renderPokemons(await getPokemons())  
+    applyBtnColor()
+    btnFunction()
 }
 
 start()
+
 
